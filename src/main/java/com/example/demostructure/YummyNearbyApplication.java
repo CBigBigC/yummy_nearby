89b1b072ed935d0a744e8f27ea2cd7b1845caa98
watson <zhihang.cao@bianlifeng.com>
2022-04-27 17:33:20 +0800

    初始化项目结构

---
+++ src/main/java/com/example/demostructure/YummyNearbyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demostructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyNearbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YummyNearbyApplication.class, args);
+    }
+
+}
